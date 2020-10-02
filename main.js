a509141e46f2dfc39617f244bdd81d813da28c1d
obeTvR9XDbUwquA6JPQhmbgaCCaiFa2rvf <namespacegenesis@gmail.com>
2020-10-02 17:21:47 +0800

    fix

--- main.js
+++ main.js
@@ -544,7 +544,7 @@ http.createServer(function(request, response) {
               <title>oxo-bulletin-site</title>
             </head>
             <body bgcolor="#8FBC8F">
-              <h1>缓存的公告</h1>
+              <h1><a href="/bulletins">缓存的公告</a></h1>
               <ul>
               ${lis}
               </ul>
